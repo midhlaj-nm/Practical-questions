634cf180172f28519b48569b39786d9e3343fad8
Midhlaj vk <midhlajrocks@gmail.com>
2025-03-07 11:25:51 +0530

    Fifth commit

--- genFunction.js
+++ genFunction.js
@@ -11,3 +11,17 @@ console.log(gen.next().value)
 
 
 //
+
+
+//Fibonacci function
+
+function* fibonacci(){
+    let a = 0, b = 1
+    while(true){
+    yield a;
+    [a , b] = [b, a + b]
+    }
+}
+ const fib = fibonacci()
+ console.log(fib.next().value)
+ console.log(fib.next().value)
