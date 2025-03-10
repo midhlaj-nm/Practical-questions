1aa2ee3e925d35dc50dc3042e6495466a839fb43
Midhlaj vk <midhlajrocks@gmail.com>
2025-03-10 10:34:30 +0530

    Updated

--- fs module/fsmodule.js
+++ fs module/fsmodule.js
@@ -1,4 +1,5 @@
 const fs = require('fs')
+const path = require('path')
 
 //create Folder
 fs.mkdir('TodaysDate',(err)=>{
@@ -10,16 +11,50 @@ fs.mkdir('TodaysDate',(err)=>{
 })
 
 //Delete folder
-fs.rmdir('TodaysDate',(err)=>{
-    if(err){
-        console.log('Error found')
-    } else {
-        console.log('Deletion completed')
-    }
-})
+// fs.rmdir('TodaysDate',(err)=>{
+//     if(err){
+//         console.log('Error found',err)
+//     } else {
+//         console.log('Deletion completed')
+//     }
+// })
 
 //list files and folders
 fs.readdir('.',(err, files) => {
     if(err) throw err;
     console.log('Directory contents:',files) 
+}) 
+
+//create a file
+fs.writeFile('TodaysDate.txt',new Date().toString(), (err)=>{
+    if(err){
+        console.log(err)
+    } else {
+        console.log('File created Successfully')
+    }
+})
+
+//read file 
+fs.readFile('TodaysDate.txt','utf8',(data,err)=>{
+    if(err){
+        console.log(err)
+    } else {
+        console.log("The contentof the page is : ",data)
+    }
+})
+
+//Delete a file
+fs.unlink('TodaysDate.t',(err)=>{
+    err ? err : 'File deleted Successfuly'
+})
+
+console.log('Process.cwd() ',process.cwd())
+console.log('Dir name ',__dirname) 
+
+//file in anothr Directory
+const dir = 'C:\\Users\\midhl\\Documents\\VScode\\JavaScript\\fs module\\TodaysDate'
+const filename = path.join(dir, 'Date.txt')
+
+fs.writeFile(filename, new Date().toString(), (err)=>{
+     return err ? err : 'File created Successfully'
 })
