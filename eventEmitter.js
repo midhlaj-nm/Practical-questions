17550246084b2617cff5bb8d7c949eb8b00cd8ba
Midhlaj vk <midhlajrocks@gmail.com>
2025-03-13 09:47:01 +0530

    updated

--- eventEmitter.js
+++ eventEmitter.js
@@ -1,4 +1,8 @@
 const EventEmitter = require('events')
 const event = new EventEmitter();
 event.on('sayhello', () => console.log('How are you!'))
-event.emit()
+event.emit('sayhello')
+
+
+event.on('hi', () => console.log('Are you fine with this'))
+event.emit('hi')
